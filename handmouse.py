351b9259adfaa909de29f670f893b80aea7b70dd
mhrisham-remian <yshanta412@gmail.com>
2025-09-30 02:21:57 +0600

    Handmouse-Hand folded and thumbs up Y-direction done, X-dir and Thumbs-up needs work

--- handmouse.py
+++ handmouse.py
@@ -1,6 +1,8 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import warnings
+warnings.filterwarnings("ignore")
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -25,7 +27,7 @@ while True:
     result = hands.process(rgb)
 
     if result.multi_hand_landmarks:
-        for hand_landmarks in result.multi_hand_landmarks:
+        for hand_landmarks, handedness in zip(result.multi_hand_landmarks, result.multi_handedness):
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
             screen_x = int(hand_landmarks.landmark[8].x * width)
@@ -34,26 +36,39 @@ while True:
             curr_x = prev_x + (screen_x - prev_x) / smoothen
             curr_y = prev_y + (screen_y - prev_y) / smoothen
 
-            pyautogui.moveTo(curr_x, curr_y)
+            #pyautogui.moveTo(curr_x, curr_y)
 
-            if hand_landmarks.landmark[8].y > hand_landmarks.landmark[6].y \
-                and hand_landmarks.landmark[12].y > hand_landmarks.landmark[10].y:
-                    pyautogui.click()
-                    pyautogui.mouseDown()
+            hand_type = handedness.classification[0].label
 
-                    cv2.putText(frame, "Fist=Left click/drag", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)  
+            index_folded_ydir = hand_landmarks.landmark[8].y > hand_landmarks.landmark[6].y
+            middle_folded_ydir = hand_landmarks.landmark[12].y > hand_landmarks.landmark[10].y
+            ring_folded_ydir = hand_landmarks.landmark[16].y > hand_landmarks.landmark[14].y
+            pinky_folded_ydir = hand_landmarks.landmark[20].y > hand_landmarks.landmark[18].y
+
+            index_folded_xdir = hand_landmarks.landmark[8].x < hand_landmarks.landmark[6].x
+            middle_folded_xdir = hand_landmarks.landmark[12].x < hand_landmarks.landmark[10].x
+            ring_folded_xdir = hand_landmarks.landmark[16].x < hand_landmarks.landmark[14].x
+            pinky_folded_xdir = hand_landmarks.landmark[20].x < hand_landmarks.landmark[18].x
+
+            if hand_type == "Right":
+                cv2.putText(frame, "Right Hand detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                thumb_open_ydir = hand_landmarks.landmark[4].y < hand_landmarks.landmark[3].y
+            else:
+                cv2.putText(frame, "Left Hand detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                thumb_open_ydir = hand_landmarks.landmark[4].y < hand_landmarks.landmark[3].y
+
+            if index_folded_ydir and middle_folded_ydir and ring_folded_ydir and pinky_folded_ydir and not thumb_open_ydir:
+                #pyautogui.click()
+                #pyautogui.mouseDown()
+                cv2.putText(frame, "Fist=Left click/drag", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)  
             else:
                 pyautogui.mouseUp() 
             
-            if (hand_landmarks.landmark[4].y < hand_landmarks.landmark[3].y and
-                hand_landmarks.landmark[8].y > hand_landmarks.landmark[6].y and
-                hand_landmarks.landmark[12].y > hand_landmarks.landmark[10].y and
-                hand_landmarks.landmark[16].y > hand_landmarks.landmark[14].y and
-                hand_landmarks.landmark[20].y > hand_landmarks.landmark[18].y):
-                pyautogui.rightClick()
-                cv2.putText(frame, "Thumbs Up = Right Click", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-    else:
-        pyautogui.mouseUp()
+            if (thumb_open_ydir and index_folded_ydir and middle_folded_ydir and ring_folded and pinky_folded_ydir ):
+                #pyautogui.rightClick()
+                cv2.putText(frame, "Thumbs Up = Right Click", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+    #else:
+        #pyautogui.mouseUp()
 
     cv2.imshow("Hand Mouse(press q to exit, Fist > Left click/drag, Thumbs Up = Right Click)", frame)
 
